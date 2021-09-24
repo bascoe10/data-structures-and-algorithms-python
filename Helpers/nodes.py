9a5cfaac381fc765de971b6251a975ba7cf2f8a2
Abass Sesay <bassco10@gmail.com>
2021-09-23 17:09:33 -0700

    nodes created for trees and linked lists

---
+++ Helpers/nodes.py
@@ -0,0 +1,26 @@
+class SimpleTreeNode:
+    def __init__(self, val, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class AdvanceTreeNode:
+    def __init__(self, val, parent=None, left=None, right=None) -> None:
+        self.val = val
+        self.parent = parent
+        self.left = left
+        self.right = right
+
+
+class SingleListNode:
+    def __init__(self, val, next=None) -> None:
+        self.val = val
+        self.next = next
+
+
+class DoubleListNode:
+    def __init__(self, val, next=None, prev=None) -> None:
+        self.val = val
+        self.next = next
+        self.prev = prev

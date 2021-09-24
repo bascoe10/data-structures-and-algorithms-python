7770268b0e44e6b972754ce58b8d1ddf4b7fa03c
Abass Sesay <bassco10@gmail.com>
2021-09-23 18:40:35 -0700

    create doubly linkedlist

--- DataStructures/linkedlists.py
+++ DataStructures/linkedlists.py
@@ -26,9 +26,41 @@ class SinglyLinkedList:
         node = SingleListNode(key)
         node.next = self.__head.next
         self.__head.next = node
+        self.__size += 1
 
     def delete(self, key):
         prev = self.__delete_search(key)
         node = prev.next
         if node is not None:
             prev.next = node.next
+            self.__size -= 1
+
+
+class DoublyLinkedList:
+    def __init__(self) -> None:
+        self.__head = DoubleListNode(None)
+        self.__size = 0
+
+    def search(self, key) -> DoubleListNode:
+        current = self.__head.next
+        while current is not None and current.val != key:
+            current = current.next
+        return current
+
+    def insert(self, key) -> None:
+        node = DoubleListNode(key)
+        node.next = self.__head.next
+        if self.__head.next is not None:
+            self.__head.next.prev = node
+        node.prev = self.__head
+        self.__head.next = node
+        self.__size += 1
+
+    def remove(self, key) -> None:
+        node = self.search(key)
+        if node is not None:
+            node.prev.next = node.next
+
+        if node.next is not None:
+            node.next.prev = node.prev
+        self.__size -= 1

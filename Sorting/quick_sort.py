9935267aac8956b97baf50899f9de1b03cf91604
Abass Sesay <bassco10@gmail.com>
2021-09-25 07:25:30 -1000

    complete randomized quick sort

--- Sorting/quick_sort.py
+++ Sorting/quick_sort.py
@@ -1,4 +1,9 @@
+import random
+import time
+
+
 def quickSort(iterable):
+
     def swap(i, j):
         if i == j:
             return
@@ -25,8 +30,42 @@ def quickSort(iterable):
     return iterable
 
 
+def randomizedQuickSort(iterable):
+    # random.seed(int(time.time))
+
+    def swap(i, j):
+        if i == j:
+            return
+        temp = iterable[i]
+        iterable[i] = iterable[j]
+        iterable[j] = temp
+
+    def randomized_partition(startidx, endidx):
+        i = random.randint(startidx, endidx)
+        swap(i, endidx)
+        return partition(startidx, endidx)
+
+    def partition(startidx, endidx):
+        pivot = iterable[endidx]
+        i = startidx - 1
+        for j in range(startidx, endidx):
+            if iterable[j] < pivot:
+                i += 1
+                swap(i, j)
+        swap(i+1, endidx)
+        return i + 1
+
+    def quick_sort(startidx, endidx):
+        if startidx < endidx:
+            mididx = randomized_partition(startidx, endidx)
+            quick_sort(startidx, mididx-1)
+            quick_sort(startidx + 1, endidx)
+    quick_sort(0, len(iterable) - 1)
+    return iterable
+
+
 def main():
-    print(quickSort([1, 8, 7, 6, 5, 4, 9, 2, 3]))
+    print(randomizedQuickSort([1, 8, 7, 6, 5, 4, 9, 2, 3]))
 
 
 if __name__ == '__main__':

f4b051a882fdaf55acc15c3832e02af94fb772d2
Abass Sesay <bassco10@gmail.com>
2021-09-25 07:27:44 -1000

    complete tail recursive quicksort

--- Sorting/quick_sort.py
+++ Sorting/quick_sort.py
@@ -64,9 +64,29 @@ def randomizedQuickSort(iterable):
     return iterable
 
 
-def main():
-    print(randomizedQuickSort([1, 8, 7, 6, 5, 4, 9, 2, 3]))
+def tailRecursiveQuickSort(iterable):
 
+    def swap(i, j):
+        if i == j:
+            return
+        temp = iterable[i]
+        iterable[i] = iterable[j]
+        iterable[j] = temp
 
-if __name__ == '__main__':
-    main()
+    def partition(startidx, endidx):
+        pivot = iterable[endidx]
+        i = startidx - 1
+        for j in range(startidx, endidx):
+            if iterable[j] < pivot:
+                i += 1
+                swap(i, j)
+        swap(i+1, endidx)
+        return i + 1
+
+    def quick_sort(startidx, endidx):
+        while startidx < endidx:
+            mididx = partition(startidx, endidx)
+            quick_sort(startidx, mididx-1)
+            startidx = mididx + 1
+    quick_sort(0, len(iterable) - 1)
+    return iterable

0cb61b8b991a7d41220ebf3c219791f1ce6937d4
Abass Sesay <bassco10@gmail.com>
2021-09-25 07:51:18 -1000

    cleanup

--- Sorting/counting_sort.py
+++ Sorting/counting_sort.py
@@ -12,11 +12,3 @@ def countingSort(A):
         B[C[A[j]]-1] = A[j]
         C[A[j]] -= C[A[j]]
     return B
-
-
-def main():
-    print(countingSort([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]))
-
-
-if __name__ == '__main__':
-    main()

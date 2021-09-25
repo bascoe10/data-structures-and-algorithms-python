66247b1c623ee007d46914fa6ee93a2b575ecb0b
Abass Sesay <bassco10@gmail.com>
2021-09-25 07:49:02 -1000

    counting sort complete, modification need for duplicate

---
+++ Sorting/counting_sort.py
@@ -0,0 +1,22 @@
+def countingSort(A):
+    k = max(A) + 1
+    C = [0] * k
+    B = [0] * len(A)
+    for j in range(len(A)):
+        C[A[j]] += 1
+
+    for i in range(1, k):
+        C[i] = C[i] + C[i-1]
+
+    for j in reversed(range(len(A))):
+        B[C[A[j]]-1] = A[j]
+        C[A[j]] -= C[A[j]]
+    return B
+
+
+def main():
+    print(countingSort([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]))
+
+
+if __name__ == '__main__':
+    main()

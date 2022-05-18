8069fcc861aa48822794c86172a472d9e0256c7c
Abass Sesay <bassco10@gmail.com>
2022-05-18 14:13:55 -0700

    keep track of arraybased stack length

--- DataStructures/stacks.py
+++ DataStructures/stacks.py
@@ -1,13 +1,16 @@
 class ArrayBasedStack:
     def __init__(self) -> None:
         self.__container = []
+        self.__length = 0
 
     def isEmpty(self) -> bool:
-        return len(self.__container) == 0
+        return self.__length == 0
 
     def push(self, item) -> None:
         self.__container.append(item)
+        self.__length += 1
 
     def pop(self) -> object:
-        if len(self.__container) > 0:
+        if self.__length > 0:
+            self.__length -= 1
             return self.__container.pop()
